aca4667319e4d997b45c60a3e22c2fa4fdab3e5f
Himanshu <yadavhimanshu63@gmail.com>
2020-11-12 10:09:44 +0530

    generating dice random value

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,17 +1,16 @@
 package com.snakeladder;
 
-public class SnakeLaddereGame {
+public class SnakeLadderGameUC2 {
 	private int player1;
-
-	 //player1 starting position
-	public int snakeLadder() {
-		player1 = 0;
-		return player1;
+	public void snakeLadder()
+	{
+		player1=0;
+		player1= (int)((Math.random()*10)%6);
+		 System.out.println("after rolling dice random value " + player1);
 	}
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Game");
-		SnakeLaddereGame game = new SnakeLaddereGame();
-		System.out.println(game.snakeLadder());
+		SnakeLadderGameUC2 game = new SnakeLadderGameUC2();
+	      game.snakeLadder();
 	}
 }
