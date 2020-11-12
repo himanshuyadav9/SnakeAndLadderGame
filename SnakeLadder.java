285f75d87df1e660a24b2fadcffe64781ea58584
Himanshu <yadavhimanshu63@gmail.com>
2020-11-12 10:36:51 +0530

    Till 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,45 +1,48 @@
 package com.snakeladder;
 
 public class SnakeLadderGame {
-	private int player1;
-	private int diceCount;
+	
+	//player1 starting point
+	private int player1 = 0;
 	private final int NOPLAY = 1;
 	private final int SNAKE = 2;
 	private final int LADDER = 3;
 
 	public void snakeLadder() {
-		//player starting position
-		player1 = 0;
 		
-		//generating random value 1 to 6
-		player1 = (int) ((Math.random() * 10) % 6);
-		System.out.println("after rolling dice random value " + player1);
-		int position=0;
-		
-		//generating random value to check
-		int option = (int) ((Math.random() * 10) % 3);
-		switch (option) {
-		case NOPLAY:
-			//no player
-			position = player1;
-			System.out.println("Player in same position " + position);
-			break;
-		case LADDER:
-			//player move ahead
-			position = player1 + diceCount;
-			System.out.println("Player move ahead  in position " + position);
-			break;
-		case SNAKE:
-			//player move behind
-			position = player1 - diceCount;
-			System.out.println("Player move behind in position " + position);
-			break;
+		while (player1 < 100) {
+			int playerPosition;
+			
+			//generating random value 1 to 6
+			int dice = (int) ((Math.random() * 10) % 6);
+			
+			//generating random value to check
+			int checkOption = (int) ((Math.random() * 10) % 3);
+			switch (checkOption) {
+			case NOPLAY:
+				//no player
+				playerPosition = player1;
+				System.out.println("Player in same position " + playerPosition);
+				break;
+			case LADDER:
+				//player move ahead
+				playerPosition = player1 + dice;
+				System.out.println("Player move ahead  in position " + playerPosition);
+				break;
+			case SNAKE:
+				//player move behind
+				playerPosition = player1 - dice;
+				if (playerPosition < 0) {
+					playerPosition = 0;
+				}
+				System.out.println("Player move behind in position " + playerPosition);
+				break;
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake Ladder Game");
-		SnakeLadderGame game = new SnakeLadderGameUC3();
+		SnakeLadderGame game = new SnakeLadderGame();
 		game.snakeLadder();
 	}
 }
